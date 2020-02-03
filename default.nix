5413b9535a4ff4fa2d58f2653b0a1014d17ea589
Moritz Angermann <moritz.angermann@gmail.com>
2020-02-03 18:40:21 +0800

    Add postgresql service to test-db test case

--- default.nix
+++ default.nix
@@ -37,6 +37,39 @@ recRecurseIntoAttrs (x: with pkgs; lib.isAttrs x && !lib.isDerivation x)
   (pkgs.haskell-nix.haskellLib.selectProjectPackages
     # from our project which is based on a cabal project.
     (pkgs.haskell-nix.cabalProject {
-        src = pkgs.haskell-nix.haskellLib.cleanGit { src = ./.; };
-        ghc = pkgs.haskell-nix.compiler.${haskellCompiler};
+      src = pkgs.haskell-nix.haskellLib.cleanGit { src = ./.; };
+      ghc = pkgs.haskell-nix.compiler.${haskellCompiler};
+      modules = [{
+        packages.cardano-explorer-db.components.tests.test-db = {
+          build-tools = [ pkgs.postgresql ];
+          preCheck = ''
+            echo pre-check
+            export PGHOST=/tmp
+            initdb --encoding=UTF8 --locale=en_US.UTF-8 --username=postgres $NIX_BUILD_TOP/db-dir
+            postgres -k $PGHOST -D $NIX_BUILD_TOP/db-dir &
+            PSQL_PID=$!
+            sleep 10
+            if (echo '\q' | psql postgres postgres); then
+              echo "PostgreSQL server is verified to be started."
+            else
+              echo "Failed to connect to local PostgreSQL server."
+              exit 2
+            fi
+            ls -ltrh $NIX_BUILD_TOP
+            DBUSER=nixbld
+            DBNAME=nixbld
+            export PGPASSFILE=$NIX_BUILD_TOP/pgpass
+            echo "/tmp:5432:$DBUSER:$DBUSER:*" > $PGPASSFILE
+            cp -vir ${./schema} ../schema
+            chmod 600 $PGPASSFILE
+            psql postgres postgres <<EOF
+              create role $DBUSER with createdb login password '$DBPASS';
+              alter user $DBUSER with superuser;
+              create database $DBNAME with owner = $DBUSER;
+              \\connect $DBNAME
+              ALTER SCHEMA public   OWNER TO $DBUSER;
+            EOF
+          '';
+        };
+      }];
     }))
